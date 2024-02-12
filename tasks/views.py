6fea5a77a98fe5a653512386ccc05c8a3f496bbe
MohammadIshak47 <ishak1998bd@gmail.com>
2024-02-12 17:42:05 +0600

    search and filtering logic has been implemented on TaskList view in tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, CreateView, DetailView, UpdateView, D
 from django.urls import reverse_lazy
 from tasks.models import Task
 from tasks.forms import TaskCreateForm,TaskUpdateForm
+from django.db.models import Q
 
 
 class TaskListView(ListView):
@@ -9,6 +10,21 @@ class TaskListView(ListView):
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
     ordering = ['-created_at']  # Sort tasks by creation date by default
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        task_status = self.request.GET.get('task_status')
+        
+        # Filter queryset based on task_status
+        if task_status:
+            queryset = queryset.filter(task_status=task_status)
+            
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
+        return queryset 
 
 class TaskCreateView(CreateView):
     model = Task
