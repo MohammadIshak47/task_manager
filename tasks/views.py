97425bc51f42ad40d6e93611b6be564d96a3aae3
MohammadIshak47 <ishak1998bd@gmail.com>
2024-02-12 15:49:17 +0600

    all class based view & templates according to the project has been created

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-# Create your views here.
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+    ordering = ['-created_at']  # Sort tasks by creation date by default
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'task_create.html'
+    fields = ['title', 'description', 'due_date', 'priority']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'task_update.html'
+    fields = ['title', 'description', 'due_date', 'priority']
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_delete.html'
+    success_url = reverse_lazy('task-list')
+
+
+
 
 
