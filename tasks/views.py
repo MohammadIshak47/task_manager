a745b87d00c421628b927795eb1188290cfa1966
MohammadIshak47 <ishak1998bd@gmail.com>
2024-02-12 16:08:16 +0600

    added URL paths for the task-related views in the tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -4,13 +4,13 @@ from .models import Task
 
 class TaskListView(ListView):
     model = Task
-    template_name = 'task_list.html'
+    template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
     ordering = ['-created_at']  # Sort tasks by creation date by default
 
 class TaskCreateView(CreateView):
     model = Task
-    template_name = 'task_create.html'
+    template_name = 'tasks/task_create.html'
     fields = ['title', 'description', 'due_date', 'priority']
 
     def form_valid(self, form):
@@ -19,16 +19,16 @@ class TaskCreateView(CreateView):
 
 class TaskDetailView(DetailView):
     model = Task
-    template_name = 'task_detail.html'
+    template_name = 'tasks/task_details.html'
 
 class TaskUpdateView(UpdateView):
     model = Task
-    template_name = 'task_update.html'
+    template_name = 'tasks/task_update.html'
     fields = ['title', 'description', 'due_date', 'priority']
 
 class TaskDeleteView(DeleteView):
     model = Task
-    template_name = 'task_delete.html'
+    template_name = 'tasks/task_delete.html'
     success_url = reverse_lazy('task-list')
 
 
