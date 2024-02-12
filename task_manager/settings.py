a6a76cef7e677425b9f36f72581a339302f3163d
MohammadIshak47 <ishak1998bd@gmail.com>
2024-02-12 20:15:25 +0600

    Api app has created . Now I am working on to create rest api

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
      # all apps 
     "tasks.apps.TasksConfig",
+    "api.apps.ApiConfig",
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
